22ab1fd63e3cb298e9066ba964fe8c3430676825
Renel Desir <renel.desir@nelnet.net>
2024-05-06 17:31:11 -0400

    add create and redirect actions

--- shortener/urls.py
+++ shortener/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello),
+    path('create', views.create, name='create'),
+    path('<str:code>', views.redirect, name='redirect'),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,52 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, JsonResponse, Http404, HttpResponseBadRequest, HttpResponseRedirect
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+from django.utils.crypto import get_random_string
+import json
 
-def say_hello(request):
-    return render(request, 'hello.html', {'name': 'World'})
+from .models import ShortUri
+
+def create(request: HttpRequest):
+    if request.method != 'POST':
+        return Http404()
+
+    # Get the destination uri
+    request_body = json.loads(request.body)
+    dest_uri = request_body.get('dest_uri', None)
+
+    if not dest_uri:
+        return HttpResponseBadRequest("dest_uri is required")
+
+    # Validate the uri
+    val = URLValidator()
+
+    try:
+        val(dest_uri)
+    except ValidationError as e:
+        return HttpResponseBadRequest(f"Invalid URI: {dest_uri}")
+
+    # Generate a random code
+    code = get_random_string(length=5)
+
+    while ShortUri.objects.filter(code=code).exists():
+        code = get_random_string(length=5)
+
+    # Save the short uri
+    shorturi = ShortUri(dest_uri=dest_uri, code=code)
+    shorturi.save()
+
+    return JsonResponse({
+        "short_uri": request.build_absolute_uri('/') + code,
+        "dest_uri": dest_uri,
+        }, status=201)
+
+def redirect(request: HttpRequest, code):
+    if request.method != 'GET':
+        return Http404()
+
+    shorturi = ShortUri.objects.filter(code=code).first()
+
+    if shorturi is None:
+        return Http404()
+
+    return HttpResponseRedirect(shorturi.dest_uri)
